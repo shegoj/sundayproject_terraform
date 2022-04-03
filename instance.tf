a0aea8ee1d22914579d3a3b398bb3c729daa42ed
shegoj <shegoj@yahoo.com>
2022-04-03 19:51:02 +0100

    syntax correction

--- instance.tf
+++ instance.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "web server" {
+resource "aws_instance" "webserver" {
   ami           = "ami-080af029940804103"
   instance_type = "t2.micro"
   count         = 1
@@ -9,7 +9,7 @@ resource "aws_instance" "web server" {
   }
 }
 
-resource "aws_instance" "app server" {
+resource "aws_instance" "appserver" {
   ami           = "ami-080af029940804103"
   instance_type = "t2.micro"
   count         = 1
